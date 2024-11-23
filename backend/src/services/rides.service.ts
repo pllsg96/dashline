48d9f0cf5ed027d0a98566657d83ed4a2c1c9e3b
pllsg96 <pedroluizlsg@gmail.com>
2024-11-23 17:29:06 -0300

    fix: drivers and review relation and first route response

--- backend/src/services/rides.service.ts
+++ backend/src/services/rides.service.ts
@@ -11,12 +11,38 @@ class RidesService {
   }
 
   public async estimateRide(origin: string, destination: string, userId: string) {
-    // console.log(origin, destination, '-------------------------')
     const from = await getCoordinates(origin)
     const to = await getCoordinates(destination)
     const routeTrajectory = await getRoute(from, to)
+    const formatedData = {
+      "origin": {
+        "latitude": from.lat,
+        "longitude": from.lng,
+      },
+      "destination": {
+        "latitude": to.lat,
+        "longitude": to.lng,
+      },
+      "distance": routeTrajectory.distanceMeters,
+      "duration": routeTrajectory.duration,
+      "options": [
+        {
+          "id": "id",
+          "name": "name",
+          "description": "description",
+          "vehicle": "vehicle",
+          "review":
+            { 
+              "rating": "rating",
+              "comment": "comment"
+            },
+          "value": "value"
+        },
+      ],
+      "routeResponse": routeTrajectory
+    }
     // const rides = await prisma.rides.findMany();
-    return { status: 200, result: routeTrajectory };
+    return { status: 200, result: formatedData };
   }
 }
 
